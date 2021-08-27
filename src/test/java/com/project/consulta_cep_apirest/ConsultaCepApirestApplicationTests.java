5aa0ee410ac1cc672e38539540504842af64c355
Lucas Silva <lucascavalcante@hotmail.com>
2021-08-27 18:56:22 -0300

    project creation

---
+++ src/test/java/com/project/consulta_cep_apirest/ConsultaCepApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.consulta_cep_apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaCepApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
